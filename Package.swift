a8ca2498af543dd5b0d5820965eee5c0511ef45d
hoge <fuga>
2026-05-30 23:07:55 +0900

    Mirror: rewrite binaryTarget URL to kyu-suke/libghostty-spm (storage.1.2.2)

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libghostty",
-            url: "https://github.com/Lakr233/libghostty-spm/releases/download/storage.1.2.2/GhosttyKit.xcframework.zip",
+            url: "https://github.com/kyu-suke/libghostty-spm/releases/download/storage.1.2.2/GhosttyKit.xcframework.zip",
             checksum: "7f712b8df5943ba02070c468de7d785abedebf207d3a3ded6515c7467309e902"
         ),
         .testTarget(
